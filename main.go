95707e3ff052d2257ab19da6739da3161c4a6329
Giofahreza <giofahreza2000@gmail.com>
2025-04-24 20:52:34 +0700

    Add refresh token

--- main.go
+++ main.go
@@ -40,6 +40,8 @@ func main() {
 
 	router.POST("/login", login)
 
+	router.POST("/refresh-token", refreshToken)
+
 	router.POST("/dashboard", authentication(dashboard))
 
 	router.POST("/user/all", authorization(authentication(getUser)))
@@ -73,6 +75,11 @@ func authorization(next httprouter.Handle) httprouter.Handle {
 
 func authentication(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		// Remove the "Bearer " prefix from the token if present
+		if len(r.Header["Authorization"]) > 0 {
+			r.Header.Set("Authorization", r.Header["Authorization"][0][7:])
+		}
+
 		// Validate JWT token here
 		token := r.Header.Get("Authorization")
 		if token == "" {
@@ -99,6 +106,18 @@ func authentication(next httprouter.Handle) httprouter.Handle {
 		}
 
 		log.Print("Authentication middleware")
+		if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
+			// Check if the token is expired
+			if exp, ok := claims["exp"].(float64); ok {
+				if time.Unix(int64(exp), 0).Before(time.Now()) {
+					http.Error(w, "Token expired", http.StatusUnauthorized)
+					return
+				}
+			}
+		} else {
+			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+			return
+		}
 		// Call the next handler if authenticated
 		next(w, r, ps)
 	}
@@ -117,7 +136,7 @@ func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		// Generate JWT token and send it in the response
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 			"username": username,
-			"exp":      jwt.TimeFunc().Add(time.Hour * 24).Unix(),
+			"exp":      jwt.TimeFunc().Add(time.Minute * 1).Unix(),
 		})
 
 		// Sign the token with your secret key
@@ -128,13 +147,90 @@ func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 			return
 		}
 
-		w.Write([]byte(tokenstring))
+		// Generate refresh token
+		refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": username,
+			"exp":      jwt.TimeFunc().Add(time.Hour * 5).Unix(),
+		})
+		jwtSecretRefresh := os.Getenv("JWT_SECRET_REFRESH_KEY")
+		refreshTokenString, err := refreshToken.SignedString([]byte(jwtSecretRefresh))
+		if err != nil {
+			http.Error(w, "Error signing refresh token", http.StatusInternalServerError)
+			return
+		}
+
+		// Return access and refresh tokens
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{"access_token": "%s", "refresh_token": "%s"}`, tokenstring, refreshTokenString)))
 
 		return
 	}
 
 	http.Error(w, "Invalid credentials", http.StatusUnauthorized)
 	return
+}
+
+func refreshToken(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// Handle refresh token logic here
+	// For example, validate the refresh token and generate a new access token
+	refreshToken := r.FormValue("refresh_token")
+
+	// Validate the refresh token
+	if refreshToken == "" {
+		http.Error(w, "Missing refresh token", http.StatusUnauthorized)
+		return
+	}
+
+	log.Println("Refresh Token:", refreshToken)
+
+	// Parse the refresh token
+	parsedToken, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		// Validate the token signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		// Return the secret key for validation
+		return []byte(os.Getenv("JWT_SECRET_REFRESH_KEY")), nil
+	})
+	if err != nil {
+		log.Println("Error parsing refresh token:", err)
+		http.Error(w, "Invalid refresh token", http.StatusUnauthorized)
+		return
+	}
+
+	log.Print("Refresh Token middleware")
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if ok && parsedToken.Valid {
+		if exp, ok := claims["exp"].(float64); ok {
+			if time.Unix(int64(exp), 0).Before(time.Now()) {
+				http.Error(w, "Refresh token expired", http.StatusUnauthorized)
+				return
+			}
+		}
+	} else {
+		http.Error(w, "Invalid refresh token claims", http.StatusUnauthorized)
+		return
+	}
+
+	// Generate a new access token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": claims["username"],
+		"exp":      jwt.TimeFunc().Add(time.Minute * 1).Unix(),
+	})
+	// Sign the new token with your secret key
+	jwtSecret := os.Getenv("JWT_SECRET_KEY")
+	tokenstring, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		http.Error(w, "Error signing new token", http.StatusInternalServerError)
+		return
+	}
+	// Return the new access token
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`{"access_token": "%s"}`, tokenstring)))
+
+	fmt.Fprintln(w, "Refresh token validated successfully!")
 }
 
 func dashboard(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
